03e03929a49c9a6be31a096d1aafe1fb4975d512
BAYMAX1314 <32646065+BAYMAX1314@users.noreply.github.com>
2017-10-23 16:58:25 +0800

    Update ruby_intro.rb

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -2,7 +2,6 @@
 
 # Part 1
 
-#part1_sum
 def sum arr
   # YOUR CODE HERE
   result = 0
@@ -14,29 +13,19 @@ def sum arr
   end  
 end
 
-#part1_max_2_sum
 def max_2_sum arr
   # YOUR CODE HERE
-  if arr.length < 1
-     return 0
+  result = 0
+  if arr.empty? then
+     return result
   elsif arr.length == 1	
      return arr[0]	
   else 	
-     max_value_1 = arr[1]	
-     max_value_2 = arr[0]
-     for i in 0..(arr.length-1)
-        x = arr[i]
-        if max_value_1 <= x
-           max_value_2 = max_value_1
-           max_value_1 = x
-        end
-     end
-     puts max_value_1 + max_value_2
-     return max_value_1 + max_value_2	
+     arr.sort!
+     return arr[0]+arr[1]
   end
 end
 
-#part1_sum_to_n
 def sum_to_n? arr, n
   # YOUR CODE HERE
   if arr.length<=1	
@@ -45,7 +34,7 @@ def sum_to_n? arr, n
      for i in 0..(arr.length-1)
         for j in i+1..(arr.length-1)
            if arr[i]+arr[j]==n
-           return true
+              return true
            end
         end
      end
@@ -55,26 +44,25 @@ end
 
 # Part 2
 
-#part2_hello
 def hello(name)
   # YOUR CODE HERE
   return 'Hello, '+ name
 end
 
-#part2_starts_with_consonant
 def starts_with_consonant? s
   # YOUR CODE HERE
-  if ( /[a-z]/ =~ s[0].downcase && /[^aeiou]/ =~ s[0].downcase)	
+  if not s =~/^[AEIOUaeiou]/ and s =~/^[A-Za-z]/	
      return true	
   else
      return false	
   end
 end
 
-#part2_binary_multiple_of_4
 def binary_multiple_of_4? s
   # YOUR CODE HERE
-  if (s.to_s =~ /\b[01]+\b/ && s.to_i % 4 == 0)
+  if s=="" || /[^01]/=~s
+     return false
+  elsif s.to_i(2)%4==0
      return true
   else	
      return false
@@ -86,9 +74,12 @@ end
 class BookInStock
 # YOUR CODE HERE
    def initialize(isbn,price)
-     raise ArgumentError if isbn == "" or price <= 0 
-     @isbn = isbn
-     @price = price
+     if isbn == "" or price <= 0
+        raise ArgumentError
+     else
+        @isbn = isbn
+        @price = price
+     end
    end	
    def isbn
      @isbn 
@@ -96,14 +87,14 @@ class BookInStock
    def price
      @price
    end
-   def isbn=(value)
-     @isbn = value	
+   def isbn=(isbn)
+     @isbn = isbn	
    end
-   def price=(value)
-     @price = value
+   def price=(price)
+     @price = price
    end	
-   def price_as_string
-     formatprice = format('%.2f', @price)
+   def price_as_string()
+     formatprice = format('%.2f', @price.round(2))
      return "$" + formatprice.to_s
    end
 end
